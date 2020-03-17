10bd67798db7c5165a3b86c45ea38c4fa518db70
pradweep <pradeep.iostech@gmail.com>
2020-03-17 19:35:33 +0530

    Initial Commit

---
+++ CommitGitLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommitGitLab
+//
+//  Created by Prime MacBook on 17/03/20.
+//  Copyright © 2020 Prime Technology Group iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
